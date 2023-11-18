2cd8ce395c1c00d5dfaaa42838d7f89f536e6349
jeannette <jett@circe.com>
2023-11-18 18:03:27 -0500

    odom setup function and get and set count implemented

--- sensors/odom.py
+++ sensors/odom.py
@@ -1,28 +1,18 @@
 from machine import Pin
-from include.rcc_pins import RCC_Pins
 
 class Odom:
-    def __init__(self, side):
-        if side == "left":
-            self.pin = Pin(RCC_Pins.RCC_LEFT_ENC, Pin.IN)
-        elif side == "right":
-            self.pin = Pin(RCC_Pins.RCC_RIGHT_ENC, Pin.IN)
-
+    def setup(self, pin_input):
+        self.pin = Pin(pin_input, Pin.IN)
         self.count = 0
         self.pin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=self.callback)
+        return f"ODOM SETUP ON {pin_input}"
 
     def callback(self, pin):
         self.count +=1
 
-    def getCount(self):
+    def get_count(self):
         return self.count
 
-    def setZero(self):
-        count = 0
-
-
-# Left_Odom = Odom("left")
-# Right_Odom = Odom("right")
-
-# while True:
-#     print(Left_Odom.getCount(), Right_Odom.getCount())
+    def reset_count(self):
+        self.count = 0
+        return self.count
