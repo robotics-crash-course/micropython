573c3ca91ed2b622b5bc94a10d850854834a2c9c
Jeannette <jett@circe.com>
2023-12-05 23:50:34 -0500

    working with des from pot

--- util/velocitycontrol.py
+++ util/velocitycontrol.py
@@ -4,6 +4,7 @@ from include.rcc_pins import Pins
 from machine import Timer
 from include.pwm_helper import Motors
 from include.rcc_library import Raft
+from util.pidcontrol import PID_Control
 
 myraft = Raft()
 myraft.setup_pot()
@@ -12,20 +13,29 @@ odom = Odom()
 odom.setup(Pins.LEFT_ODOM)
 
 vel = Differentiator(0.1, 0.02)
+velcontroller = PID_Control(3, 0.1, 0, 0.02, 0.1, 0, 100, False, True)
+
 
 velocity = 0
 count = 0
+controller_output = 0
 
 def vel_callback(timer):
-    global velocity
+    global velocity, controller_output
     velocity = vel.differentiate(odom.get_count())
-    print(odom.get_count(), velocity)
+
+    scaled_pot = (myraft.get_pot()/65535)*150
+    des_vel = int(scaled_pot)
+    controller_output = velcontroller.pid(des_vel, velocity)
+    mymotors.set_power(int(controller_output), 0)
+
+    print(odom.get_count(), velocity, des_vel, controller_output)
 
 veltimer = Timer(mode=Timer.PERIODIC, period=20, callback=vel_callback)
 
 mymotors = Motors()
 mymotors.setup()
 
+
 while True:
-    scaled_power = (myraft.get_pot()/65535)*100
-    mymotors.set_power(int(scaled_power), 0)
+    pass
