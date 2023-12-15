6e9ca2ffb771a8a07b0b00230c0c94a99bb7e1e8
Jeannette <jett@circe.com>
2023-12-15 18:38:42 -0500

    updates to odom lib to allow for directional encoder

--- sensors/odom.py
+++ sensors/odom.py
@@ -16,3 +16,27 @@ class Odom:
     def reset_count(self):
         self.count = 0
         return self.count
+    
+
+class Directional_Odom:
+    def setup(self, dir_pin_input, int_pin_input):
+        self.count = 0
+        self.dir_pin = Pin(dir_pin_input)
+        self.int_pin = Pin(int_pin_input)
+        self.int_pin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=self.callback)
+
+    def callback(self, pin):
+        self.pin_1 = self.int_pin.value()
+        self.pin_2 = self.dir_pin.value()
+
+        if self.pin_1 ^ self.pin_2:
+            self.count -= 1
+        else:
+            self.count += 1
+
+    def get_count(self):
+        return self.count
+    
+    def reset_count(self):
+        self.count = 0
+        return self.count
